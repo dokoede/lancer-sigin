aa1738055949378e186f7a659f26ffd0160dd121
dokoede <dennisokoede@outlook.com>
2019-10-08 14:22:35 +0100

    color mod

--- main.js
+++ main.js
@@ -1,18 +1,22 @@
 const login = document.querySelector("#loginform");
-const email = document.querySelector("#email").value;
 const password = document.querySelector("#password").value;
+// const passwordMessage = document.querySelector("#pmessage");
 
 login.addEventListener("submit", function(e) {
   e.preventDefault();
   passReg = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z])\w{6,}/;
-
-  if (email === "" || email == null) {
-    alert("Please enter valid Email");
-  }
+  const passwordMessage = document.querySelector("#pmessage");
 
   if (password != passReg) {
-    alert(
-      "Password must have more than six characters, digits 0-9, No spaces or special character"
-    );
+    passwordMessage.textContent = null;
+    let p = document.createElement(`p`);
+    p.classList.add("alert", "alert-warning");
+    p.textContent = "Please enter password. Letters or digits Only";
+    passwordMessage.appendChild(p);
+    console.log(p);
+
+    setTimeout(function() {
+      passwordMessage.innerHTML = "";
+    }, 4000);
   }
 });
